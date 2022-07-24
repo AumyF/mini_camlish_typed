dcf1c1e7a6be7f5bb8096b1afe2d813138a7a3b9
Aumy <aumy@outlook.jp>
2022-07-24 19:46:31 +0900

    implement minimal general type inference

--- TypeChecker.fs
+++ TypeChecker.fs
@@ -19,7 +19,12 @@ let ok_or e =
   | Some (v) -> Ok(v)
   | None -> Error(e)
 
-let typeVar s = TVar("'" + s)
+let mutable n = 0
+
+let newTypeVar () =
+  let name = "'a" + n.ToString()
+  n <- n + 1
+  TVar(name)
 
 let substitute tvar t tenv =
   option {
@@ -28,61 +33,7 @@ let substitute tvar t tenv =
   }
   |> Option.defaultValue tenv
 
-let rec typecheck1 tenv expr =
-  match expr with
-  | IdentifierReference (name) ->
-    match lookup tenv name with
-    | Some (ty) -> Ok(tenv, ty)
-    | None ->
-      let tvar = typeVar name
-      let te1 = tenv.Add(name, tvar)
-      Ok(te1, tvar)
-  | IntegerLiteral (_) -> Ok(tenv, TInt)
-  | BoolLiteral (_) -> Ok(tenv, TBool)
-  | Add (e1, e2) ->
-
-    let inferAndExtend t tenv =
-      match t with
-      | TInt -> Ok(tenv)
-      | TVar (s) -> Ok(substitute t TInt tenv)
-      | _ -> Error("type error in add")
-
-    result {
-      let! (te1, t1) = typecheck1 tenv e1
-      let! te2 = inferAndExtend t1 te1
-      let! (te3, t2) = typecheck1 te2 e2
-      let! te4 = inferAndExtend t2 te3
-      return (te4, TInt)
-    }
-
-  | If (econd, etrue, efalse) ->
-    let inferAndExtendPredicate t tenv =
-      match t with
-      | TBool -> Ok(tenv)
-      | TVar (s) -> Ok(substitute t TBool tenv)
-      | _ -> Error("Expected bool")
-
-    let inferAndExtendBody tt tf tenv =
-      match (tt, tf) with
-      | (TInt, TInt) -> Ok(tenv, TInt)
-      | (TBool, TBool) -> Ok(tenv, TBool)
-      | (TInt, TVar (s)) -> Ok(substitute tf TInt tenv, TInt)
-      | (TVar (s), TInt) -> Ok(substitute tt TInt tenv, TInt)
-      | (TBool, TVar (s)) -> Ok(substitute tf TBool tenv, TBool)
-      | (TVar (s), TBool) -> Ok(substitute tt TBool tenv, TBool)
-      | (TVar (s), TVar (t)) ->
-        let tenv = substitute tt tf tenv
-        Ok(tenv, tf)
-      | _ -> Error("type error in if")
-
-    result {
-      let! (tenv, tcond) = typecheck1 tenv econd
-      let! tenv = inferAndExtendPredicate tcond tenv
-      let! (tenv, ttrue) = typecheck1 tenv etrue
-      let! (tenv, tfalse) = typecheck1 tenv efalse
-      let! (tenv, tresult) = inferAndExtendBody ttrue tfalse tenv
-      return (tenv, tresult)
-    }
+let theta0 = ([]: TySubst)
 
 let rec occurs tx t =
   if tx = t then
@@ -105,7 +56,7 @@ let rec subst_ty theta t =
   | TVar (s) -> subst_ty1 theta s
 
 let subst_tyenv theta tenv =
-  List.map (fun (x, t) -> (x, subst_ty theta t)) tenv
+  Map.map (fun x t -> (subst_ty theta t)) tenv
 
 let subst_eql theta eql =
   List.map (fun (t1, t2) -> (subst_ty theta t1, subst_ty theta t2)) eql
@@ -155,36 +106,113 @@ let unify eql =
 
   solve eql []
 
+let rec typecheck1 tenv expr =
+  let mutable typeVarCount = 0
+  let newTypeVar () =
+    let name= "'a" + n.ToString()
+    n <- n + 1
+    TVar(name)
+  match expr with
+  | IdentifierReference (name) ->
+    match lookup tenv name with
+    | Some (ty) -> Ok(tenv, ty, theta0)
+    | None ->
+      let tvar = newTypeVar ()
+      let tenv = Map.add name tvar tenv
+      Ok(tenv, tvar, theta0)
+  | IntegerLiteral (_) -> Ok(tenv, TInt, theta0)
+  | BoolLiteral (_) -> Ok(tenv, TBool, theta0)
+  | Add (eleft, eright) ->
+    result {
+      let! (tenv, tleft, theta1) = typecheck1 tenv eleft
+      let! (tenv, tright, theta2) = typecheck1 tenv eright
+      let tleft = subst_ty theta2 tleft
+      let! theta3 = unify [ (tleft, TInt); (tright, TInt) ]
+      let tenv = subst_tyenv theta3 tenv
+      let theta = compose_subst theta3 (compose_subst theta2 theta1)
+      return (tenv, TInt, theta)
+    }
+  | Function (param, ebody) ->
+    result {
+      let tparam = newTypeVar ()
+      let tenv = Map.add param tparam tenv
+      let! (tenv, tbody, theta1) = typecheck1 tenv ebody
+      let tparam = subst_ty theta1 tparam
+      let tenv = Map.remove param tenv
+      return (tenv, TFn(tparam, tbody), theta1)
+    }
+  | Apply (efn, earg) ->
+    result {
+      let! (tenv, tfn, theta1) = typecheck1 tenv efn
+      let! (tenv, targ, theta2) = typecheck1 tenv earg
+      let tvar = newTypeVar ()
+      let tfn = subst_ty theta2 tfn
+      let! theta3 = unify [ (tfn, TFn(targ, tvar)) ]
+      let tret = subst_ty theta3 tvar
+      let tenv = subst_tyenv theta3 tenv
+      let theta4 = compose_subst theta3 (compose_subst theta2 theta1)
+      return (tenv, tret, theta4)
+    }
+  | _ -> Error("unknown expression")
+
+
+//| If (econd, etrue, efalse) ->
+//result {
+//  let! (tenv, tcond, theta1) = typecheck1 tenv econd
+// let! (tenv, ttrue, theta2) = typecheck1 tenv etrue
+//let! (tenv, tfalse, theta3) = typecheck1 tenv efalse
+// let ttrue = subst_ty theta3 ttrue
+// let! theta4= unify
+// }
+
+let rec showType ty =
+  match ty with
+  | TInt -> "int"
+  | TBool -> "bool"
+  | TFn(p, b) ->
+    let p = showType p
+    let b = showType b
+    sprintf "%s -> %s" p b
+  | TVar(name) -> name
+
+let checktop e = typecheck1 (Map []) e
 
 let test tenv expr =
   printfn "Typechecking %A:" expr
   let result = typecheck1 tenv expr
-  printfn "  %A" result
+  match result with
+  | Ok(tenv, texpr, theta) -> printfn "  %A" (showType texpr)
+  | Error(_) -> printfn "  %A" result
 
 let test_unify eql =
   printfn "Unifying %A:" eql
   let result = unify eql
   printfn "  %A" result
+    
 
 [<EntryPoint>]
 let main argv =
-  let tenv = Map []
-
-  [ If(BoolLiteral(true), IntegerLiteral(1), IntegerLiteral(100))
-    If(BoolLiteral(true), IntegerLiteral(1), BoolLiteral(true)) ]
-  |> List.iter (test tenv)
-
-  let tenv = Map []
-
-  [ If(IdentifierReference("p"), IntegerLiteral(1), IdentifierReference("x"))
-    If(IdentifierReference("x"), IntegerLiteral(3), IntegerLiteral(12)) ]
-  |> List.iter (test tenv)
-
-  [ [ (TVar("'a"), TBool) ]
-    [ (TInt, TBool) ]
-    [ (TVar("'a"), TVar("'b")) ]
-    [ (TFn(TVar("'a"), TVar("'b")), TFn(TVar("'b"), TVar("'c"))) ]
-    [ (TVar("'a"), TFn(TVar("'b"), TVar("'a"))) ] ]
-  |> List.iter (test_unify)
+  [
+    // If(BoolLiteral true, IntegerLiteral 1, IntegerLiteral 100)
+    // If(IdentifierReference "x", Add(IdentifierReference "y", IntegerLiteral 10), IntegerLiteral 100) ;
+    Function("a", Function("b", Add(IdentifierReference "a", IdentifierReference "b")))
+    Function(
+      "f",
+      Function("x", Apply(IdentifierReference "f", Apply(IdentifierReference "f", IdentifierReference "x")))
+    )
+    Function(
+      "x",
+      Function(
+        "y",
+        Function(
+          "z",
+          Apply(
+            Apply(IdentifierReference "x", IdentifierReference "z"),
+            Apply(IdentifierReference "y", IdentifierReference "z")
+          )
+        )
+      )
+    ) ]
+  |> List.iter (test (Map []))
 
   0
