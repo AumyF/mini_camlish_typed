e47e0b0b09a2594bbcc365c7fe94fb3a8c15a777
Aumy <aumy@outlook.jp>
2022-07-29 07:30:39 +0900

    Add a test for typecheck

--- MiniCamlishTyped.Tests/TypeCheckerTest.fs
+++ MiniCamlishTyped.Tests/TypeCheckerTest.fs
@@ -3,6 +3,7 @@ module MiniCamlishTyped.Tests
 open System
 open Parser
 open TypeChecker
+open FsToolkit.ErrorHandling
 open NUnit.Framework
 open FsUnitTyped
 
@@ -35,3 +36,20 @@ let ``Succeeds to unify function types`` () =
 let ``Fails to unify because of `occurs` `` () =
   unify [ TVar "a", TFn(TVar "b", TVar "a") ]
   |> shouldEqual (Error "unification failed: t1 occurs in t2")
+
+[<Test>]
+let Test2 () =
+  result {
+    let! (_, ty, _) =
+      typecheck1
+        (Map [])
+        (Function(
+          "f",
+          Function("x", Apply(IdentifierReference "f", Apply(IdentifierReference "f", IdentifierReference "x")))
+        ))
+
+    return
+      ty
+      |> shouldEqual (TFn(TFn(TVar "'a3", TVar "'a3"), TFn(TVar "'a3", TVar "'a3")))
+  }
+  |> ignore
